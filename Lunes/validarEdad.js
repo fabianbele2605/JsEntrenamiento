0344df9545bb1b51b7e65edbd41ff5e4b5159bc2
fabian beleño <fabianrobles321@outlook.com>
2025-06-17 09:08:14 -0500

    actualize nuevo

--- Lunes/validarEdad.js
+++ Lunes/validarEdad.js
@@ -1,6 +1,6 @@
 // Validar si una edad permite conducir
 
-// creamos la function
+/* creamos la function
 function puedesConducir(edad) {
 
     if(edad >= 16){
@@ -8,9 +8,32 @@ function puedesConducir(edad) {
     }else{
         console.log("No tienes edad para conducir")
     }
-}
+} 
 
 // aqui llamamos la function "puedesConducir" 
 // y le damos el valor del argumento que es la edad
 puedesConducir(18)
+*/
 
+// aca hay otro ejemplo mucho mejor
+
+edad = 20
+
+function edadParaConducir(edad) {
+    try {
+        if (!Number.isInteger(edad)){
+            throw new Error("Ingresa un numero")
+        }
+
+        if (edad >= 16){
+            console.log("Tienes edad para conducir")
+        }
+        else{
+            console.log("No tienes edad para conducir")
+        }
+    } catch (error) {
+        throw new Error (error.message)
+    }
+}
+
+edadParaConducir(edad)
